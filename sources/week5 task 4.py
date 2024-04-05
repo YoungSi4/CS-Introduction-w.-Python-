494587c7afb7f2b4ad3a5a391a4bfb96ad718c9a
YoungSi4 <dksdudgns01@gmail.com>
2024-04-05 15:36:23 +0900

    week5 task 4

--- sources/week5 task 4.py
+++ sources/week5 task 4.py
@@ -2,7 +2,7 @@ from cs1robots import *
 
 load_world("worlds/harvest3.wld")
 
-hubo = Robot(beepers=6) # 해당 월드에 beeper를 둘 곳이 6개밖에 없으니까
+hubo = Robot()
 hubo.set_trace("blue")
 hubo.set_pause(0.1)
 
@@ -10,3 +10,31 @@ hubo.set_pause(0.1)
 def turn_right():
     for _ in range(3):
         hubo.turn_left()
+
+
+def move_and_pick():
+    hubo.move()
+    if hubo.on_beeper():
+        hubo.pick_beeper()
+
+
+def one_cycle():
+    for _ in range(5):
+        move_and_pick()
+    hubo.turn_left()
+    move_and_pick()
+    hubo.turn_left()
+    for _ in range(5):
+        move_and_pick()
+
+
+if __name__ == "__main__":
+    move_and_pick()
+
+    for _ in range(2):
+        one_cycle()
+        turn_right()
+        move_and_pick()
+        turn_right()
+
+    one_cycle()
