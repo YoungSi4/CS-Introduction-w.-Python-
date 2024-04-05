9d1e11a55b08c030f0cbaaa2354d889ed38cbe1e
YoungSi4 <dksdudgns01@gmail.com>
2024-04-05 13:34:14 +0900

    week5 task 1

--- sources/week5 task 1.py
+++ sources/week5 task 1.py
@@ -2,11 +2,11 @@
 # input으로 사용자 입력하지 말고 변수를 tuple로 초기화해서 사용할 것
 # x값과 y값을 비교해서 조건을 분기해주자
 
-a = (5, -5)
-b = (-5, 5)
+a = (5, 5)
+b = (5, 5)
 
 if a[0] == b[0] and a[1] == b[1]:
-    print("same")
+    print("same points")
 elif a[0] == -b[0] and a[1] == b[1]:
     print("Y-axis symmetry")
 elif a[0] == b[0] and a[1] == -b[1]:
