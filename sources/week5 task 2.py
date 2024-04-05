1ec1717bc5e786810f52e662db71a968932d074a
YoungSi4 <dksdudgns01@gmail.com>
2024-04-05 14:10:02 +0900

    week5 task 2

--- sources/week5 task 2.py
+++ sources/week5 task 2.py
@@ -1,1 +1,14 @@
 # 분기를 잘 고려하여 if문의 사용을 최소화 할 것!
+
+# 가위 바위 보
+# 3 Pi 2 = 9
+
+player1 = "보"
+player2 = "보"
+
+if player1 == player2:
+    print("draw")
+elif (player1 == "바위" and player2 == "가위") or (player1 == "가위" and player2 == "보") or (player1 == "보" and player2 == "바위"):
+    print("player1")
+else:
+    print("player2")
